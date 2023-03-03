43b53f73bb7e249629ad64ee6f75e0f8bd7ced53
ViniciusDinizz <viniciusdiniz.ti/2gmail.com>
2023-03-03 15:11:30 -0300

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float N1, N2;
+        Console.WriteLine("Digite um número: ");
+        N1 = float.Parse(Console.ReadLine());
+        Console.WriteLine("Digite outro número: ");
+        N2 = float.Parse(Console.ReadLine());
+        Console.WriteLine("A soma dos valores é: " + (N1 + N2));
+        Console.WriteLine("A subtração dos valores é: " + (N1 - N2));
+        Console.WriteLine("A multiplicação dos valores é: " + (N1 * N2));
+        if (N2 == 0)
+        {
+            Console.WriteLine("Ímpossivel dividir por zero!");
+        }
+        else
+        {
+            Console.WriteLine("A divisão enre o valores vale: " + (N1 / N2));
+        }
+    }
+}
